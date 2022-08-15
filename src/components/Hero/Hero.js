b638efd524195b825366d871e97627d5ca92ef5e
Surjo Seth <54148263+SurjoSeth@users.noreply.github.com>
2022-08-16 00:38:07 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -18,7 +18,7 @@ const Hero = (props) => (
         My Portfolio
       </SectionTitle>
       <SectionText>
-      Aspiring to be a part of a reputed organization, working in a responsible position and performing consistently to the best of my skills and abilities in order to learn and grow along with the organization. Currently pursuing B. Tech in Computer Science and Engineering from MCKV Institute of Engineering under Maulana Abul Kalam Azad University of Technology.
+          Welcome to my personal portfolio.
       </SectionText>
       <Button onClick={saveFile}>Download my CV</Button>
     </LeftSection>
