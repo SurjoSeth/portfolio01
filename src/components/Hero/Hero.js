f456e3bd74de0a8f07fd7bcbae875bba74bd7fc9
Surjo Seth <54148263+SurjoSeth@users.noreply.github.com>
2022-08-16 00:44:15 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,11 +14,10 @@ const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Welcome To <br />
-        My Portfolio
-      </SectionTitle>
+        Hello,<br />
+        I am Surjo Seth.
+  </SectionTitle>
       <SectionText>
-          Hello, I am Surjo Seth. <br />
           Welcome to my personal portfolio.
       </SectionText>
       <Button onClick={saveFile}>Download my CV</Button>
