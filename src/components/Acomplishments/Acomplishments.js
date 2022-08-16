accc3be8f438538baa3a7b4e5f02ff94e9d13cf0
Surjo Seth <54148263+SurjoSeth@users.noreply.github.com>
2022-08-16 18:14:37 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -11,7 +11,7 @@ const data = [
 ];
 
 const Acomplishments = () => (
-  <Section>
+  <Section id='accomp'>
     <SectionTitle>Acievements & Extra Curricular Activities</SectionTitle>
     <Boxes>
       {data.map((card,index) =>(
